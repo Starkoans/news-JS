60398fa7a8a2011010d9dfcc831aec7f4eafb5d5
Anastasiia <starkoans@gmail.com>
2025-02-17 13:16:25 +0500

    feat: tsconfig, eslint for ts

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ const baseConfig = {
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.js', '.ts'],
     },
     output: {
         filename: 'index.js',
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,24 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    devtool: 'inline-source-map',
+    entry: './src/index.ts',
+    devtool: 'inline-source-map', //чтобы при ошибках показывался оригинальный код
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    // output: {
+    //     filename: 'bundle.js',
+    //     path: path.resolve(__dirname, 'dist'),
+    // },
     devServer: {
         static: path.resolve(__dirname, './dist'),
     },
